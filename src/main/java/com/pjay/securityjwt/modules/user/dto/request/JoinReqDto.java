ed08cf190885593cfc2e24dda461a9e600740c13
qwq140 <qwq140@gmail.com>
2023-03-07 14:11:40 +0900

    Feat : 회원가입 유효성 검사 완료

--- src/main/java/com/pjay/securityjwt/modules/user/dto/request/JoinReqDto.java
+++ src/main/java/com/pjay/securityjwt/modules/user/dto/request/JoinReqDto.java
@@ -7,17 +7,34 @@ import lombok.Setter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
+/**
+ * username
+ * 영문, 숫자, 길이 최소 2~20자 이내
+ * password
+ * 길이 4~20
+ * email
+ * 이메일 형식
+ * fullname
+ * 영문, 한글, 길이 1~20
+ */
 @Getter
 @Setter
 public class JoinReqDto{
+
     @NotEmpty // null 이거나 공백일 수 없다.
+    @Pattern(regexp = "^[a-zA-Z0-9]{2,20}$", message = "영문/숫자 2~20자 이내로 작성해주세요")
     private String username;
     @NotEmpty
+    @Size(min = 4, max = 20)
     private String password;
     @NotEmpty
+    @Pattern(regexp = "[a-zA-Z0-9]{2,10}@[a-zA-Z0-9]{2,6}\\.[a-zA-Z]{2,3}", message = "이메일 형식으로 작성해주세요")
     private String email;
     @NotEmpty
+    @Pattern(regexp = "^[a-zA-Z가-힣]{1,20}$", message = "한글/영문 1~20자 이내로 작성해주세요")
     private String fullname;
 
     public User toEntity(BCryptPasswordEncoder passwordEncoder){
