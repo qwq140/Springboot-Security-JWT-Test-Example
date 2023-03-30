8582b538677d1eaffe7a9e13a0b9ce4fba4fb0d9
qwq140 <qwq140@gmail.com>
2023-03-30 13:12:22 +0900

    Feat : 계좌등록 서비스 만들기

--- src/main/java/com/pjay/securityjwt/modules/account/domain/AccountRepository.java
+++ src/main/java/com/pjay/securityjwt/modules/account/domain/AccountRepository.java
@@ -2,5 +2,13 @@ package com.pjay.securityjwt.modules.account.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    // jpa query method
+    // select * from account where number = :number
+    // TODO : 리팩토링 해야함
+    Optional<Account> findByNumber(Long number);
+
 }
---
+++ src/main/java/com/pjay/securityjwt/modules/account/service/AccountService.java
@@ -0,0 +1,78 @@
+package com.pjay.securityjwt.modules.account.service;
+
+import com.pjay.securityjwt.handler.ex.CustomApiException;
+import com.pjay.securityjwt.modules.account.domain.Account;
+import com.pjay.securityjwt.modules.account.domain.AccountRepository;
+import com.pjay.securityjwt.modules.user.domain.User;
+import com.pjay.securityjwt.modules.user.domain.UserRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public AccountSaveRespDto addAccount(Long userId, AccountSaveReqDto accountSaveReqDto){
+        // User가 DB에 있는지 검증
+        User userPS = userRepository.findById(userId).orElseThrow(
+                () -> new CustomApiException("유저를 찾을 수 없습니다.")
+        );
+
+        // 해당 계좌가 DB에 있는지 중복여부를 체크
+        Optional<Account> accountOP = accountRepository.findByNumber(accountSaveReqDto.getNumber());
+        if(accountOP.isPresent()){
+            throw new CustomApiException("해당 계좌가 이미 존재합니다.");
+        }
+        // 계좌 등록
+        Account accountPS = accountRepository.save(accountSaveReqDto.toEntity(userPS));
+
+        // DTO를 응답
+        return new AccountSaveRespDto(accountPS);
+    }
+
+    @Getter
+    @Setter
+    public static class AccountSaveRespDto{
+        private Long id;
+        private Long number;
+        private Long balance;
+
+        public AccountSaveRespDto(Account account) {
+            this.id = account.getId();
+            this.number = account.getNumber();
+            this.balance = account.getBalance();
+        }
+    }
+
+    @Getter
+    @Setter
+    public static class AccountSaveReqDto{
+        @NotNull
+        @Digits(integer = 4, fraction = 4)
+        private Long number;
+        @NotNull
+        @Digits(integer = 4, fraction = 4)
+        private Long password;
+
+        public Account toEntity(User user){
+            return Account.builder()
+                    .number(number)
+                    .password(password)
+                    .balance(1000L)
+                    .user(user)
+                    .build();
+        }
+    }
+}
