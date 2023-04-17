20fc94ffc2139b738b45f9efd4bd9525cfdd1143
qwq140 <qwq140@gmail.com>
2023-04-17 13:27:39 +0900

    Test : 계좌등록 컨트롤러 테스트

---
+++ src/test/java/com/pjay/securityjwt/modules/account/controller/AccountControllerTest.java
@@ -0,0 +1,66 @@
+package com.pjay.securityjwt.modules.account.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pjay.securityjwt.config.dummy.DummyObject;
+import com.pjay.securityjwt.modules.account.dto.request.AccountSaveReqDto;
+import com.pjay.securityjwt.modules.user.domain.User;
+import com.pjay.securityjwt.modules.user.domain.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@Transactional
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class AccountControllerTest extends DummyObject {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp(){
+        User user = userRepository.save(newUser("pjay", "피제이"));
+    }
+
+    // jwt token -> 인증필터 -> 시큐리티 세션생성
+    // setupBefore=TEST_METHOD (setUp 메서드 실행전에 수행)
+    // setupBefore = TEST_EXECUTION (saveAccount_test 메서드 실행전에 수행)
+    @WithUserDetails(value = "pjay", setupBefore = TestExecutionEvent.TEST_EXECUTION) // 디비에서 username = pjay 조회를 해서 세션에 담아주는 어노테이션
+    @Test
+    public void saveAccount_test() throws Exception {
+        // given
+        AccountSaveReqDto accountSaveReqDto = new AccountSaveReqDto();
+        accountSaveReqDto.setNumber(9999L);
+        accountSaveReqDto.setPassword(1234L);
+        String requestBody = om.writeValueAsString(accountSaveReqDto);
+        System.out.println("테스트 : "+requestBody);
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/api/s/account").content(requestBody).contentType(MediaType.APPLICATION_JSON));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : "+responseBody);
+
+        // then
+        resultActions.andExpect(status().isCreated());
+    }
+}
