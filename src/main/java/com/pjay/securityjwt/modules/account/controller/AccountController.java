6002ba891d3d9228da8d2804d38397173685934b
qwq140 <qwq140@gmail.com>
2023-04-17 13:59:23 +0900

    Feat : 본인계좌목록조회 컨트롤러 만들기

--- src/main/java/com/pjay/securityjwt/modules/account/controller/AccountController.java
+++ src/main/java/com/pjay/securityjwt/modules/account/controller/AccountController.java
@@ -3,10 +3,10 @@ package com.pjay.securityjwt.modules.account.controller;
 import com.pjay.securityjwt.common.ResponseDto;
 import com.pjay.securityjwt.config.auth.LoginUser;
 import com.pjay.securityjwt.modules.account.dto.request.AccountSaveReqDto;
+import com.pjay.securityjwt.modules.account.dto.response.AccountListRespDto;
 import com.pjay.securityjwt.modules.account.dto.response.AccountSaveRespDto;
 import com.pjay.securityjwt.modules.account.service.AccountService;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -27,5 +27,12 @@ public class AccountController {
         AccountSaveRespDto accountSaveRespDto = accountService.addAccount(loginUser.getUser().getId(), accountSaveReqDto);
 
         return new ResponseEntity<>(new ResponseDto<>(1, "계좌등록 성공.", accountSaveRespDto), HttpStatus.CREATED);
+    }
+
+    // 인증이 필요하고, account 테이블에 login한 유저의 계좌만 주세요.
+    @GetMapping("/s/account/login-user")
+    public ResponseEntity<?> findUserAccount(@AuthenticationPrincipal LoginUser loginUser){
+        AccountListRespDto accountListRespDto = accountService.accountListByUser(loginUser.getUser().getId());
+        return new ResponseEntity<>(new ResponseDto<>(1, "계조목록조회 성공", accountListRespDto), HttpStatus.OK);
     }
 }
