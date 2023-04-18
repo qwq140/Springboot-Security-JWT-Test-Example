fbfe034c853a773f4579d2daa8dbc40e03e48a84
qwq140 <qwq140@gmail.com>
2023-04-18 14:19:58 +0900

    Test : 본인계좌목록조회 서비스 테스트

--- src/test/java/com/pjay/securityjwt/modules/account/service/AccountServiceTest.java
+++ src/test/java/com/pjay/securityjwt/modules/account/service/AccountServiceTest.java
@@ -5,6 +5,7 @@ import com.pjay.securityjwt.config.dummy.DummyObject;
 import com.pjay.securityjwt.modules.account.domain.Account;
 import com.pjay.securityjwt.modules.account.domain.AccountRepository;
 import com.pjay.securityjwt.modules.account.dto.request.AccountSaveReqDto;
+import com.pjay.securityjwt.modules.account.dto.response.AccountListRespDto;
 import com.pjay.securityjwt.modules.account.dto.response.AccountSaveRespDto;
 import com.pjay.securityjwt.modules.user.domain.User;
 import com.pjay.securityjwt.modules.user.domain.UserRepository;
@@ -15,6 +16,8 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -61,5 +64,30 @@ public class AccountServiceTest extends DummyObject {
         System.out.println("테스트 : "+responseBody);
         // then
         assertThat(accountSaveRespDto.getNumber()).isEqualTo(1111L);
+    }
+
+    @Test
+    public void accountListByUserTest() throws Exception {
+        // given
+        Long userId = 1L;
+
+        // stub
+        User user = newMockUser(userId, "pjay", "피제이");
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        List<Account> accountList = new ArrayList<>();
+        for(int i = 1; i <= 3 ; i++){
+            accountList.add(newMockAccount(Long.valueOf(i), Long.valueOf(1110+i), user, 1000L));
+        }
+        when(accountRepository.findByUser_Id(userId)).thenReturn(accountList);
+
+        // when
+        AccountListRespDto accountListRespDto = accountService.accountListByUser(userId);
+        String responseBody = om.writeValueAsString(accountListRespDto);
+        System.out.println("테스트 : "+responseBody);
+
+        // then
+        assertThat(accountListRespDto.getFullname()).isEqualTo("피제이");
+        assertThat(accountListRespDto.getAccounts().size()).isEqualTo(3);
     }
 }
