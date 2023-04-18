4901f6316747a9617a445a92807eac2269088445
qwq140 <qwq140@gmail.com>
2023-04-18 14:33:24 +0900

    Test : 본인계좌목록조회 컨트롤러 테스트

--- src/test/java/com/pjay/securityjwt/modules/account/controller/AccountControllerTest.java
+++ src/test/java/com/pjay/securityjwt/modules/account/controller/AccountControllerTest.java
@@ -2,7 +2,10 @@ package com.pjay.securityjwt.modules.account.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pjay.securityjwt.config.dummy.DummyObject;
+import com.pjay.securityjwt.modules.account.domain.Account;
+import com.pjay.securityjwt.modules.account.domain.AccountRepository;
 import com.pjay.securityjwt.modules.account.dto.request.AccountSaveReqDto;
+import com.pjay.securityjwt.modules.account.dto.response.AccountListRespDto;
 import com.pjay.securityjwt.modules.user.domain.User;
 import com.pjay.securityjwt.modules.user.domain.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,6 +21,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -37,9 +44,14 @@ public class AccountControllerTest extends DummyObject {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AccountRepository accountRepository;
+
     @BeforeEach
     public void setUp(){
-        User user = userRepository.save(newUser("pjay", "피제이"));
+        User pjay = userRepository.save(newUser("pjay", "피제이"));
+        accountRepository.save(newAccount(1111L, pjay));
+        accountRepository.save(newAccount(2222L, pjay));
     }
 
     // jwt token -> 인증필터 -> 시큐리티 세션생성
@@ -62,5 +74,19 @@ public class AccountControllerTest extends DummyObject {
 
         // then
         resultActions.andExpect(status().isCreated());
+    }
+
+    @WithUserDetails(value = "pjay", setupBefore = TestExecutionEvent.TEST_EXECUTION) // 디비에서 username = pjay 조회를 해서 세션에 담아주는 어노테이션
+    @Test
+    public void findByUserAccount_test() throws Exception {
+        // given
+
+        // when
+        ResultActions resultActions = mvc.perform(get("/api/s/account/login-user"));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : "+responseBody);
+
+        // then
+        resultActions.andExpect(status().isOk());
     }
 }
