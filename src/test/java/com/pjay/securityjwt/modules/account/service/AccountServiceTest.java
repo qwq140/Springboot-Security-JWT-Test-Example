1f1dc22c03682894f0ea6490e52dd1ed9d6012b4
qwq140 <qwq140@gmail.com>
2023-04-20 11:33:47 +0900

    Test : 서비스 테스트 고찰

--- src/test/java/com/pjay/securityjwt/modules/account/service/AccountServiceTest.java
+++ src/test/java/com/pjay/securityjwt/modules/account/service/AccountServiceTest.java
@@ -132,13 +132,14 @@ public class AccountServiceTest extends DummyObject {
 
 
         // stub 1
-        User user = newMockUser(1L, "pjay", "피제이");
-        Account account1 = newMockAccount(1L, 1111L, user, 1000L); // account1 -> 1000원
+        User user1 = newMockUser(1L, "pjay", "피제이");
+        Account account1 = newMockAccount(1L, 1111L, user1, 1000L); // account1 -> 1000원
         when(accountRepository.findByNumber(any())).thenReturn(Optional.of(account1)); // 실행안됨
 
 
         // stub 2 (스텁이 진행될 때 마다 연관된 객체는 새로 만들어서 주입하기 - 타이밍 때문에 꼬인다)
-        Account account2 = newMockAccount(1L, 1111L, user, 1000L);
+        User user2 = newMockUser(1L, "pjay", "피제이");
+        Account account2 = newMockAccount(1L, 1111L, user2, 1000L);
         Transaction transaction = newMockDepositTransaction(1L, account2); // account1 -> 1100원, transaction -> 1100원
         when(transactionRepository.save(any())).thenReturn(transaction); // 실행안됨
 
@@ -146,9 +147,33 @@ public class AccountServiceTest extends DummyObject {
         AccountDepositRespDto accountDepositRespDto = accountService.deposit(accountDepositReqDto); // 여기서 when 이 실행됨
         System.out.println(accountDepositRespDto.getTransaction().getDepositAccountBalance());
         System.out.println(account1.getBalance());
+        String responseBody = om.writeValueAsString(accountDepositRespDto);
+        System.out.println("테스트 : "+responseBody);
 
         // then
         assertThat(account1.getBalance()).isEqualTo(1100L);
         assertThat(accountDepositRespDto.getTransaction().getDepositAccountBalance()).isEqualTo(1100L);
+    }
+
+
+    // 위에서는 정석적인 서비스 테스트(테크닉)
+    // 진짜 서비스 테스트를 하고 싶으면, 내가 지금 무엇을 여기서 테스트해야할지 명확히 구분(책임 분리)
+    // DTO를 만드는 책임 -> 서비스에 있지만 서비스에서 DTO검증 안할래! - Controller 테스트 해볼 것이니까
+    // DB관련된 것도 -> 서비스 것이 아니다.
+    // DB관련된 것을 조회했을 때, 그 값을 통해서 어떤 비지니스 로직이 흘러가는 것이 있으면 -> stub으로 정의해서 테스트 해보면 된다.
+    @Test
+    public void depositTest2() throws Exception {
+        // given
+        Account account = newMockAccount(1L, 1111L, null, 1000L);
+        Long amount = 0L;
+        // when
+        if(amount <= 0L){
+            throw new CustomApiException("0원 이하의 금액을 입력할 수 없습니다");
+        }
+
+        account.deposit(100L);
+
+        // then
+        assertThat(account.getBalance()).isEqualTo(1100L);
     }
 }
