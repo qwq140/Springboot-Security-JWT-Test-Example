10ad2e6ee0b660da5efed1963f918f8fda0e251f
qwq140 <qwq140@gmail.com>
2023-06-13 14:53:27 +0900

    Feat : 동적쿼리 구현
    - 계좌내역 조회 쿼리 구현(입금, 출금, 입출금)

---
+++ src/main/java/com/pjay/securityjwt/modules/transaction/domain/TransactionRepositoryImpl.java
@@ -0,0 +1,58 @@
+package com.pjay.securityjwt.modules.transaction.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+interface Dao {
+    List<Transaction> findTransactionList(@Param("accountId") Long accountId, @Param("gubun") String gubun, @Param("page") Integer page);
+}
+
+
+// Impl을 꼭 붙여줘야 하고, TransactionRepository가 앞에 붙어야 한다.
+@RequiredArgsConstructor
+public class TransactionRepositoryImpl implements Dao{
+
+    private final EntityManager em;
+
+    @Override
+    public List<Transaction> findTransactionList(Long accountId, String gubun, Integer page) {
+        // JPQL
+        String sql = "";
+        sql += "select t from Transaction t ";
+
+        if(gubun.equals("WITHDRAW")){
+            sql += "join fetch t.withdrawAccount wa ";
+            sql += "where t.withdrawAccount.id = :withdrawAccountId";
+        } else if(gubun.equals("DEPOSIT")){
+            sql += "join fetch t.depositAccount da ";
+            sql += "where t.depositAccount.id = :depositAccountId";
+        } else {
+            sql += "left join fetch t.withdrawAccount wa ";
+            sql += "left join fetch t.depositAccount da ";
+            sql += "where t.withdrawAccount.id = :withdrawAccountId ";
+            sql += "or ";
+            sql += "t,depositAccount.id = :depositAccountId";
+        }
+
+        // createQuery
+        TypedQuery<Transaction> query = em.createQuery(sql, Transaction.class);
+
+        if(gubun.equals("WITHDRAW")){
+            query = query.setParameter("withdrawAccountId", accountId);
+        } else if(gubun.equals("DEPOSIT")){
+            query = query.setParameter("depositAccountId", accountId);
+        } else {
+            query = query.setParameter("withdrawAccountId", accountId);
+            query = query.setParameter("depositAccountId", accountId);
+        }
+
+        query.setFirstResult(page * 5); // 5, 10, 15
+        query.setMaxResults(5);
+
+        return query.getResultList();
+    }
+}
